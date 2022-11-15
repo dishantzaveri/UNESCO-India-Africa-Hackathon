cc5572f0fc62765e8af8e3ecb330d66758f6fc20
Arshia-Aggarwal <arshiaaggarwal0420@gmail.com>
2022-11-15 23:01:29 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const methodOverride = require("method-override");
+var path = require("path");
+const bodyParser = require("body-parser");
+dotenv.config();
+
+// MIDDLEWARE;
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+app.use(bodyParser.json());
+app.use(methodOverride("_method"));
+app.use(express.json());
+var corsOptions = {
+  // in: null,
+  methods: "GET, PUT",
+  // optionsSuccessStatus: 200, // For legacy browser support
+};
+app.use(cors());
+// app.use(express.static("public"));
+app.use(express.json());
+
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("connected to db")
+);
+
+//ROUTES MIDDLEWARE
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Server Up and running")
+);
